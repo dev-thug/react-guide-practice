e2b35d3d81e210832fc56a4f376af1f7fa6cfdb8
HJ <de0978@gmail.com>
2022-01-23 21:43:46 +0900

    add manage userlist using state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import User from "./components/Users/User";
 import {useState} from "react";
-import NewUser from "./components/NewUser/NewUser";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
-const DUMMY_USER = [
-    {
-        id: "u1",
-        name: "김현중",
-        age: "28"
-    }
-]
-
 function App() {
 
-    const [users, setUsers] = useState(DUMMY_USER);
+    const [usersList, setUsersList] = useState([]);
 
 
-    const addUserHandler = (user) => {
-        setUsers(prevState => {
-            return [user, ...prevState]
+    const addUserHandler = (uName, uAge) => {
+        setUsersList(prevState => {
+            return [...prevState, {name: uName, age: uAge, id: Math.random().toString()}];
         })
     }
 
 
     return (
         <div>
-            <AddUser/>
-            <UsersList users={users}/>
+            <AddUser onAddUser={addUserHandler}/>
+            <UsersList users={usersList}/>
         </div>
     );
 }
