4803bfd15aa78459acb68258946136e96d3541d3
HJ <de0978@gmail.com>
2022-01-25 21:32:34 +0900

    add react portal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,7 +2,7 @@ import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import {Fragment, useState} from "react";
-import ErrorModal from "../UI/ErrorModal";
+import ErrorModal from "../../../../../hj-yejin-dev/src/components/UI/ErrorModal";
 
 const AddUser = (props) => {
 
