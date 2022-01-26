bdc8d9b267fc531112117fd55c70fce05b5e4877
HJ <de0978@gmail.com>
2022-01-26 16:17:51 +0900

    add study for reducer&context-api project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import {Fragment, useState} from "react";
-import AddUser from "./components/Users/AddUser";
-import UsersList from "./components/Users/UsersList";
+import React, { useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const [usersList, setUsersList] = useState([]);
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
 
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
 
-    const addUserHandler = (uName, uAge) => {
-        setUsersList(prevState => {
-            return [...prevState, {name: uName, age: uAge, id: Math.random().toString()}];
-        })
-    }
-
-
-    return (
-        <Fragment>
-            <AddUser onAddUser={addUserHandler}/>
-            <UsersList users={usersList}/>
-        </Fragment>
-    );
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
